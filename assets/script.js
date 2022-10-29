991e2d213d19e9dce3d43b6408196ae8f22e702b
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2022-10-28 18:49:14 -0700

    sending most recent code to learning assistant

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,44 @@
+// "Generate Password" button
 var generateBtn = document.querySelector("#generate");
 
-// Strings with the criteria
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Event listener for when user presses generateBtn to call generatePassword function, which initiates criteria prompts
+generateBtn.addEventListener("click", writePassword);
+
+
+// //prompts for user criteria selection
+// var passLength    = prompt("How many characters would you like your password to be? Please select a number between 8 and 128.");
+// var upperSelect   = confirm("Would you like to include uppercase letters?");
+// var lowerSelect   = confirm("Would you like to include lowercase letters?");
+// var numbersSelect = confirm("Would you like to include numbers?");
+// var specialSelect = confirm("Would you like to include special characters?")
+
+
+// Strings with the criteria users select from
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890"
 var special = "@#$%^&*()";
 
-let passwordChosen =[]
-let userAnswers = []
+// string that will be created with content generator can choose from, based on users' choices
+var userAnswers = []
 
-
+//Function for users' password length
 var generatePassword = function() {
   // User chooses length of password
+  var passwordLength = prompt("How many characters would you like your password to be? Please select a character amount between 8 and 128");
+    if(Number(passwordLength > 7 && passwordLength.value < 129)){
+    userChoices(passwordLength);
 
-  let pLength = window.prompt("Please select a character amount between 8 and 128");
-  if(Number(pLength > 7 && pLength < 129)){
-   userChoices(pLength)
-  }else{
-    alert('Length must be between 8 and 128.')
+    } else {
+    alert('Length must be between 8 and 128 charatcters.');
+    prompt("How many characters would you like your password to be? Please select a character amount between 8 and 128");
+
   }
-
-  while(!Number.isInteger(pLength)) {
-    alert('Please select a whole number.')
-  }
-
 }
 
-let userChoices = (pLength) => {
-  let upperSelection = window.confirm("Would you like uppercase letters?");
+// Functions for users to set the characters they want to include in their password
+var userChoices = (passwordLength) => {
+  var upperSelection = confirm("Would you like uppercase letters?");
   if(upperSelection){
    userAnswers.push(uppercase)
   }
@@ -45,13 +56,24 @@ let userChoices = (pLength) => {
   }
 
   console.log(userAnswers)
+
+  // Prompt that shows if user chooses 0 criteria
   if(userAnswers.length === 0){
-    alert('we are having a problem')
+    alert('You must select at least one character type. Please try again.')
     return;
   }
 
 }
 
+// Password generating function
+let passwordChosen =''; 
+  for(var i = 0; passwordLength.valueOf; i++) {
+    var userChoicesIndex = Math.floor(Math.random() * userChoices.length);
+    var selection = userAnswers[userChoicesIndex];
+    password += selection[Math.floor(Math.random() * selection.length)];
+}
+
+console.log(passwordChosen)
 
 // Write password to the #password input
 function writePassword() {
@@ -62,5 +84,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
